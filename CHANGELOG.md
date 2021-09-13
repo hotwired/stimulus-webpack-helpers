1659913af87bf36da65b0826c61c6cea3568fd68
Guillaume Briday <guillaumebriday@gmail.com>
2021-09-13 13:54:53 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+Please see [our GitHub "Releases" page](https://github.com/hotwired/stimulus-webpack-helpers/releases).
---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+
+import resolve from "@rollup/plugin-node-resolve"
+import typescript from "@rollup/plugin-typescript"
+import { version } from "./package.json"
+
+const year = new Date().getFullYear()
+const banner = `/*\nStimulus ${version}\nCopyright © ${year} Basecamp, LLC\n */`
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      name: "StimulusWebpackHelpers",
+      file: "dist/stimulus-webpack-helpers.umd.js",
+      format: "umd",
+      banner
+    },
+    {
+      file: "dist/stimulus-webpack-helpers.js",
+      format: "es",
+      banner
+    },
+  ],
+  context: "window",
+  plugins: [
+    resolve(),
+    typescript()
+  ]
+}
